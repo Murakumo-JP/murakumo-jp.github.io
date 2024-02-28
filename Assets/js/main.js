0d39df7bfb5cbc7aa36f51538cdb4d7d690187c3
Murakumo <32967450+Murakumo-JP@users.noreply.github.com>
2024-02-28 20:53:59 +0300

    Full Redesign

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,29 +1,27 @@
 /* Preloader */
-document.body.onload = function() {
-	setTimeout(function(){
+document.body.onload = function () {
+	setTimeout(function () {
 		var preloader = document.getElementById("page-preloader");
-		if(!preloader.classList.contains("done"))
-		{
+		if (!preloader.classList.contains("done")) {
 			preloader.classList.add("done");
 		}
-	}, 1000);
-}
+	}, 500);
+};
 // Back to Top
 function backToTop() {
-	let button = $(".nome_app_top")
- 
+	let button = $(".nome_app_top");
+
 	$(window).on("scroll", () => {
-	  if ($(this).scrollTop() >= 200) {
-		 button.fadeIn()
-	  } else {
-		 button.fadeOut()
-	  }
-	})
- 
-	button.on("click", e => {
-	  e.preventDefault()
-	  $("html").animate({ scrollTop: 0 }, 1000)
-	})
- }
- 
- backToTop()
+		if ($(this).scrollTop() >= 200) {
+			button.fadeIn();
+		} else {
+			button.fadeOut();
+		}
+	});
+
+	button.on("click", (e) => {
+		e.preventDefault();
+		$("html").animate({ scrollTop: 0 }, 1000);
+	});
+}
+//backToTop()
